83286c2aa7ec2a8ec65cfddee032080fdb3f0ec4
Martijn Braam <martijn@brixit.nl>
2026-03-20 18:31:48 +0000

    use arecord as audio input instead of ffmpeg

--- mncam/audio.sh
+++ mncam/audio.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-srcdev=$(arecord -l  |grep -E 'hifiberry' |cut -d: -f1 |cut -d' ' -f 2)
-
-ffmpeg -nostats -nostdin -f alsa -i hw:$srcdev \
-       -af astats=metadata=1:reset=1,ametadata=print:key=lavfi.astats.1.RMS_level,ametadata=print:key=lavfi.astats.2.RMS_level \
-       -f alsa hdmi:CARD=vc4hdmi0,DEV=0 2>&1 | grep RMS
+arecord -D hw:CARD=sndrpihifiberry,DEV=0 -c 2 -r 48000 -f S16_LE | ffmpeg -acodec pcm_s16le -i - -ac 2 \
+  -af astats=metadata=1:reset=1,ametadata=print:key=lavfi.astats.1.RMS_level,ametadata=print:key=lavfi.astats.2.RMS_level \
+ -f alsa hdmi:CARD=vc4hdmi0,DEV=0
